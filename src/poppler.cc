ec2a53900b124b015ee598eb6ae62a52667039c8
AIkorsky <aikorsky@gmail.com>
2020-07-20 22:27:50 +0300

    Fix for poppler 0.83

--- src/poppler.cc
+++ src/poppler.cc
@@ -7,7 +7,11 @@ using namespace v8;
 using namespace node;
 
 NAN_MODULE_INIT(InitAll) {
+#if POPPLER_VERSION_MAJOR == 0 && POPPLER_VERSION_MINOR < 83
     globalParams = new GlobalParams();
+#else
+    globalParams = std::unique_ptr<GlobalParams>(new GlobalParams);
+#endif
     NodePopplerPage::Init(target);
     NodePopplerDocument::Init(target);
 }
